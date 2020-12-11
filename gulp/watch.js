3ca4ad25793237f2df18b10b5c90e42c58a7ea48
Gabriel Rosales <gabriel.rosales0129@gmail.com>
2020-12-10 19:42:47 -0600

    First commit

---
+++ gulp/watch.js
@@ -0,0 +1,45 @@
+// grab our gulp packages
+const gulp = require("gulp");
+const gutil = require("gulp-util");
+const combine = require("gulp-scss-combine");
+const concat = require("gulp-concat");
+const watch = require("gulp-watch");
+const babel = require("gulp-babel");
+const changed = require("gulp-changed");
+const rename = require("gulp-rename");
+const uglify = require("gulp-uglify");
+
+const handleError = (error) => {
+  console.log(error.toString());
+  this.emit("end");
+};
+
+const DEST = "assets/";
+
+gulp.task("combine_css", (done) => {
+  gulp
+    .src([
+      //Vendors
+      
+    
+      //Custom
+      "assets/styles/custom.scss",
+    ])
+    .pipe(combine())
+    .pipe(concat("theme.scss.liquid"))
+    .on("error", handleError)
+    .pipe(gulp.dest(DEST));
+  done();
+});
+
+gulp.task(
+  "watch",
+  gulp.series(
+    gulp.parallel("combine_css"),
+    (done) => {
+      gulp.watch("assets/styles/**/*.scss", gulp.parallel("combine_css"));
+      
+      done();
+    }
+  )
+);
